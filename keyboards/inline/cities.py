5c9880a7424ca67b81590ef5eb41006a4aa9c091
Artur Shafiev <artur170899@gmail.com>
2023-04-17 11:17:46 +0500

    inline buttons create

---
+++ keyboards/inline/cities.py
@@ -0,0 +1,9 @@
+from telebot import types
+from typing import Dict
+
+
+def city_buttons(cities: Dict) -> types.InlineKeyboardMarkup:
+    markup = types.InlineKeyboardMarkup()
+    for city_name, city_id in cities.items():
+        markup.add(types.InlineKeyboardButton(city_name, callback_data=city_id))
+    return markup
