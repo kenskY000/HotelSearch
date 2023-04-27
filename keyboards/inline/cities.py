6e4babbebbf4d46984272306a2e04de9db4a0f16
Artur Shafiev <artur170899@gmail.com>
2023-04-28 00:56:26 +0500

    formatted to pep8 79 symbols length

--- keyboards/inline/cities.py
+++ keyboards/inline/cities.py
@@ -5,5 +5,8 @@ from typing import Dict
 def city_buttons(cities: Dict) -> types.InlineKeyboardMarkup:
     markup = types.InlineKeyboardMarkup()
     for city_name, city_id in cities.items():
-        markup.add(types.InlineKeyboardButton(city_name, callback_data=city_id))
+        markup.add(types.InlineKeyboardButton(city_name,
+                                              callback_data=city_id))
     return markup
+
+
