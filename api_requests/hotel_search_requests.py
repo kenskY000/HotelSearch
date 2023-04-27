4745aa8e0b94e40b21aa7fc2022e365f8d6053c3
Artur Shafiev <artur170899@gmail.com>
2023-04-28 00:57:50 +0500

    added requests for handlers

--- api_requests/hotel_search_requests.py
+++ api_requests/hotel_search_requests.py
@@ -1,8 +1,7 @@
+from typing import List
 import requests
 import os
 import json
-
-SEARCH_URL = "https://hotels4.p.rapidapi.com/locations/v3/search"
 
 
 headers = {
@@ -12,8 +11,101 @@ headers = {
 
 
 def cities_request(city: str):
+    url = "https://hotels4.p.rapidapi.com/locations/v3/search"
     querystring = {"q": city, "locale": "ru_RU"}
-    response = requests.request("GET", SEARCH_URL, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers,
+                                params=querystring, timeout=10)
 
     response_json = json.loads(response.text)
     return response_json
+
+
+def hotels_request_custom(region_id: str, check_in_date: dict, check_out_date: dict,
+                   adults: int, children: list, min_price: int, max_price: int):
+
+    url = "https://hotels4.p.rapidapi.com/properties/v2/list"
+    payload = {
+        "currency": "USD",
+        "eapid": 1,
+        "locale": "ru_RU",
+        "siteId": 300000001,
+        "destination": {"regionId": region_id},
+        "checkInDate": check_in_date,
+        "checkOutDate": check_out_date,
+        "rooms": [
+            {
+                "adults": adults,
+                "children": children
+            }
+        ],
+        "resultsStartingIndex": 0,
+        "resultsSize": 10,
+        "sort": "PRICE_LOW_TO_HIGH",
+        "filters": {
+            "price": {
+                "max": max_price,
+                "min": min_price
+            },
+            'availableFilter': 'SHOW_AVAILABLE_ONLY'
+        }
+    }
+
+    response = requests.request("POST", url, json=payload,
+                                headers=headers, timeout=10)
+    response_json = json.loads(response.text)
+    return response_json
+
+
+def hotels_request(region_id: str, check_in_date: dict, check_out_date: dict,
+                   adults: int, children: list, sort_method: str):
+
+    url = "https://hotels4.p.rapidapi.com/properties/v2/list"
+    payload = {
+        "currency": "RUB",
+        "eapid": 1,
+        "locale": "ru_RU",
+        "siteId": 300000001,
+        "destination": {"regionId": region_id},
+        "checkInDate": check_in_date,
+        "checkOutDate": check_out_date,
+        "rooms": [
+            {
+                "adults": adults,
+                "children": children
+            }
+        ],
+        "resultsStartingIndex": 0,
+        "resultsSize": 10,
+        "sort": sort_method,
+        "filters": {'availableFilter': 'SHOW_AVAILABLE_ONLY'}
+    }
+
+    response = requests.request("POST", url, json=payload,
+                                headers=headers, timeout=10)
+    response_json = json.loads(response.text)
+    return response_json
+
+
+def images_request(hotel: str) -> List:
+    url = "https://hotels4.p.rapidapi.com/properties/v2/detail"
+
+    payload = {
+        "currency": "RUB",
+        "eapid": 1,
+        "locale": "ru_RU",
+        "siteId": 300000001,
+        "propertyId": hotel
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    response_json = json.loads(response.text)
+
+    images_list = []
+    for item in range(5):
+        image_url = (
+            response_json['data']['propertyInfo']['propertyGallery']
+            ['images'][item]['image']['url']
+        )
+        images_list.append(image_url)
+
+    return images_list
