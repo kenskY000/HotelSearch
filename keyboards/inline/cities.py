3e087dae0c03c23395afe23d02f09e0d1ebf3eac
Artur Shafiev <artur170899@gmail.com>
2023-05-12 16:28:34 +0500

    spelling correction

--- keyboards/inline/cities.py
+++ keyboards/inline/cities.py
@@ -6,7 +6,7 @@ def city_buttons(cities: Dict) -> types.InlineKeyboardMarkup:
     """
     Функция создающая инлайновую клавиатуру для уточнения города
     :param cities: Словарь городов, состоящий из пары Город: ID города
-    :return: Markup объект, тобишь клавитура.
+    :return: Markup объект, т.е. клавиатура.
     """
     markup = types.InlineKeyboardMarkup()
     for city_name, city_id in cities.items():
