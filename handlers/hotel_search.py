d1dc6afead36f0a86d7e3156cd17ad8e565b779e
Artur Shafiev <artur170899@gmail.com>
2023-05-12 16:51:35 +0500

    few fixes

--- handlers/hotel_search.py
+++ handlers/hotel_search.py
@@ -43,15 +43,19 @@ def get_location(message: Message) -> None:
             city_id = response['sr'][i_var]['gaiaId']
             cities_dict[city_name] = city_id
 
-    bot.send_message(
-        message.from_user.id,
-        'Уточните локацию',
-        reply_markup=cities.city_buttons(cities_dict)
-    )
+    if len(cities_dict) > 0:
+        bot.send_message(
+            message.from_user.id,
+            'Уточните локацию',
+            reply_markup=cities.city_buttons(cities_dict)
+        )
 
-    bot.set_state(message.from_user.id,
-                  HotelSearchState.check_in,
-                  message.chat.id)
+        bot.set_state(message.from_user.id,
+                      HotelSearchState.check_in,
+                      message.chat.id)
+    else:
+        bot.send_message(message.from_user.id,
+                         'Локация не найдена! Попробуйте ещё раз')
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data.isdigit(),
