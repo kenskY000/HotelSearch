be8b56720a8aa7b9a8f0fbc5f76bb05da17ce69d
Artur Shafiev <artur170899@gmail.com>
2023-05-12 16:21:23 +0500

    added docstrings

--- keyboards/inline/cities.py
+++ keyboards/inline/cities.py
@@ -3,6 +3,11 @@ from typing import Dict
 
 
 def city_buttons(cities: Dict) -> types.InlineKeyboardMarkup:
+    """
+    Функция создающая инлайновую клавиатуру для уточнения города
+    :param cities: Словарь городов, состоящий из пары Город: ID города
+    :return: Markup объект, тобишь клавитура.
+    """
     markup = types.InlineKeyboardMarkup()
     for city_name, city_id in cities.items():
         markup.add(types.InlineKeyboardButton(city_name,
