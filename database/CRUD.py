d766561613e51e1178bbd25fac6dbbcc998d8ca1
Artur Shafiev <artur170899@gmail.com>
2023-05-12 16:17:06 +0500

    added docstrings

--- database/CRUD.py
+++ database/CRUD.py
@@ -8,11 +8,24 @@ T = TypeVar('T')
 
 
 def _store_data(db: db, model: T, *data: List[Dict]) -> None:
+    """
+    Скрытая функция для записи каких либо данных в базу данных
+    :param db: База, в которую нужно внести запись
+    :param model: Таблица в которую нужно внести запись
+    :param data: Данные которые необходимо записать
+    """
     with db.atomic():
         model.insert_many(*data).execute()
 
 
 def _retrieve_all_data(db: db, model: T, *columns: ModelBase) -> ModelSelect:
+    """
+    Функция для получения каких либо данных из базы.
+    :param db: База из которой необходимо получить информацию
+    :param model: Таблица из которой необходимо получить запись
+    :param columns: Количество записей которые необходимо получить
+    :return: Возвращает таблицу
+    """
     with db.atomic():
         response = model.select(*columns)
 
